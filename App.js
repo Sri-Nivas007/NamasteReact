cd13f2e1d7c6dc7aad31f2fcd2e6d93fa0f0e80a
DESKTOP-PK024EU\srinivas <srinivas.node07@gmail.com>
2023-10-16 01:36:20 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+const head = (
+    <div >
+      <h1>kwsfasfcfwwsdfc</h1>
+    </div>
+  );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(head);
